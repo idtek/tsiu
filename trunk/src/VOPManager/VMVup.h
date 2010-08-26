cbe4fef6d5575d7fb35130f55f3b1e76c3b39568
finytang <finytang@4a5e036e-445b-5e6c-b66d-d24271e8d589>
2010-08-26 10:18:29 +0000

    upate vup manager

--- trunk/src/VOPManager/VMVup.h
+++ trunk/src/VOPManager/VMVup.h
@@ -1,9 +1,65 @@
 #ifndef __VM_VUP_H__
 #define __VM_VUP_H__
 
+#include "VMGlobalDef.h"
+
 class VMVup
 {
+public:
+	class VupStatus
+	{
+	public:
+		enum EVupStatus
+		{
+			EVupStatus_Invalid,
+			EVupStatus_Ready,
+			EVupStatus_Running,
+			EVupStatus_Success,
+			EVupStatus_Failed,
 
+			EVupStatus_Num
+		};
+
+		VupStatus()
+			: m_eStatus(EVupStatus_Invalid)
+			, m_strStatusName()
+		{}
+		VupStatus(EVupStatus _status, StringPtr _name)
+			: m_eStatus(_status)
+			, m_strStatusName(_name)
+		{}
+		StringPtr	GetName()	const {	return m_strStatusName.c_str();	}
+		EVupStatus	GetStatus() const {	return m_eStatus;				}
+	
+		friend class VMVup;
+
+	private:
+		EVupStatus	m_eStatus;
+		std::string m_strStatusName;
+	};
+
+public:
+	static s32 GenerateUniqueID(){
+		static s32 i = 0;
+		return i++;
+	}
+	//static VupStatus kStatus[VupStatus::EVupStatus_Num];
+
+public:
+	VMVup(s32 _id);
+	VMVup(s32 _id, VupStatus::EVupStatus _status);
+
+	VMVup& operator = (const VMVup& _rhs);
+	s32 GetUniqueID() const{
+		return m_iUniqueID;
+	}
+	VupStatus::EVupStatus GetCurrentStatus() const{
+		return m_eCurrentStatus;
+	}
+
+private:
+	s32						m_iUniqueID;
+	VupStatus::EVupStatus	m_eCurrentStatus;
 };
 
 #endif
