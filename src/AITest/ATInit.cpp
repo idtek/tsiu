3d1605a9e273ce8fe92060a17fa2ad8a91fb6ed3
finytang <finytang@4a5e036e-445b-5e6c-b66d-d24271e8d589>
2011-05-24 03:38:23 +0000

    update refvalue, add more function

--- src/AITest/ATInit.cpp
+++ src/AITest/ATInit.cpp
@@ -7,36 +7,14 @@ SimpleRenderObjectUtility*	g_poSROU = NULL;
 #define WRITABLE_APP
 
 #ifdef WRITABLE_APP
-class TestObject : public Object
-{
-public:
-	TestObject()
-		: m_X("x", 0)
-		, m_Y("y", 0)
-	{
-	}	
-	virtual void Create()
-	{}
-	virtual void Tick(f32 _fDeltaTime)
-	{
-		AI::RefValueManager::Get().Flush();
-	}
-	void SetNewXY(int x, int y)
-	{
-		m_X = x;
-		m_Y = y;
-	}
-private:
-	AI::RefValue<int, AI::ERefValuFlag_Writable> m_X;
-	AI::RefValue<int, AI::ERefValuFlag_Writable> m_Y;
-};
-#else
 class TestObject : public DrawableObject
 {
 public:
 	TestObject()
-		: m_X("x", 0)
-		, m_Y("y", 0)
+		: m_X("x1", 0)
+		, m_Y("y1", 0)
+		, m_cX("x2", 0)
+		, m_cY("y2", 0)
 	{
 	}	
 	virtual void Create()
@@ -47,11 +25,52 @@ public:
 	}
 	virtual void Draw()
 	{
-		g_poSROU->DrawFillCircle((f32)m_X.As(), (f32)m_Y.As(), 10, D_Color(255, 0, 0), D_Color(255, 0, 0));
+		g_poSROU->DrawFillCircle((f32)m_cX.As(), (f32)m_cY.As(), 10, D_Color(0, 255, 0), D_Color(0, 255, 0));
+	}
+	void SetNewXY(int x, int y)
+	{
+		m_X = x;
+		m_Y = y;
 	}
 private:
-	AI::RefValue<int, AI::ERefValuFlag_ReadOnly> m_X;
-	AI::RefValue<int, AI::ERefValuFlag_ReadOnly> m_Y;
+	AI::RFInt m_X;
+	AI::RFInt m_Y;
+	AI::RFCInt m_cX;
+	AI::RFCInt m_cY;
+};
+#else
+class TestObject : public DrawableObject
+{
+public:
+	TestObject()
+		: m_cX("x1", 0)
+		, m_cY("y1", 0)
+		, m_X("x2", 0)
+		, m_Y("y2", 40)
+	{
+	}	
+	virtual void Create()
+	{}
+	virtual void Tick(f32 _fDeltaTime)
+	{
+		AI::RefValueManager::Get().Flush();
+
+		m_X = m_X.As() + (100 * _fDeltaTime);
+		if(m_X.As() > GameEngine::GetGameEngine()->GetRenderMod()->GetRenderer()->GetWidth())
+		{
+			m_X = 0;
+		}
+	}
+	virtual void Draw()
+	{
+		g_poSROU->DrawFillCircle((f32)m_X.As(), (f32)m_Y.As(), 10, D_Color(0, 255, 0), D_Color(0, 255, 0));
+		g_poSROU->DrawFillCircle((f32)m_cX.As(), (f32)m_cY.As(), 10, D_Color(255, 0, 0), D_Color(255, 0, 0));
+	}
+private:
+	AI::RFCInt m_cX;
+	AI::RFCInt m_cY;
+	AI::RFInt m_X;
+	AI::RFInt m_Y;
 };
 #endif
 class TestWindowMsgCallBack : public RenderWindowMsgListener
